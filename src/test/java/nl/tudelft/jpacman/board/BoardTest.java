45c4f676b9a1bd1b43696010cdffc51ceafae35e
Sunwei Wang <S.Wang-11@student.tudelft.nl>
2019-05-08 11:38:12 +0200

    2.8 BoardTest

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,33 +1,47 @@
 package nl.tudelft.jpacman.board;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-
-
-
-public class BoardTest {
-
-    private Square s1;
-    private Board b1;
-
+/**
+ * Tests the class invariant of {@link Board}.
+ */
+class BoardTest {
     /**
-     * Setup of the boardtests.
-     */
-    @BeforeEach
-    public void setUp() {
-        s1 = new BasicSquare();
-    }
-
-    /**
-     * Check whether the constructor properly creates a board when supplied with proper arguments.
+     * Tests the construction of a regular small board.
      */
     @Test
-    public void nonNullBoardTest() {
-        b1 = new Board(new Square[][]{{s1}});
-        assertThat(b1.squareAt(0, 0)).isEqualTo(s1);
+    void testBoardConstruction() {
+        Square square = new BasicSquare();
+
+        Board board = new Board(new Square[][] {{square}});
+
+        assertThat(board.squareAt(0, 0)).isEqualTo(square);
     }
 
+    /**
+     * Tests the construction of a board with a grid containing <code>null</code>
+     * instead of a Square.
+     *
+     * If assertions are enabled, this should result in an assertion error.
+     */
+    @Test
+    void testBoardConstructionWithNullSquareShouldThrowAssertionError() {
+        assumeTrue(assertionsEnabled());
 
+        assertThatThrownBy(() -> new Board(new Square[][] {{null}}))
+            .isInstanceOf(AssertionError.class);
+    }
+
+    /**
+     * Checks if assertions are enabled.
+     *
+     * @return true iff assertions are enabled
+     */
+    static boolean assertionsEnabled() {
+        return BoardTest.class.desiredAssertionStatus();
+    }
 }
